d693230eaf2aaccc874b771750ae80f38ff17e09
Kusu <slgd@airmail.cc>
2019-06-12 00:58:16 -0600

    Remove unnecessary headers, fix indentation

--- main.cpp
+++ main.cpp
@@ -1,12 +1,4 @@
-#include <mutex>
-#include <iostream>
-#include <chrono>
-#include <thread>
-#include <future>
 #include <string>
-#include <cstdio>
-#include <cstdlib>
-#include <cstdint>
 
 extern "C" {
 #include <linux/input.h>
@@ -28,42 +20,42 @@ inline void write_event(const input_event &event) {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 7)
-        return std::fprintf(
-                   stderr,
-                   "usage: %s <x res> <y res> <output display x-offset> <output display y-offset> <x pos> <y pos>\n",
-                   argv[0]),
-               EXIT_FAILURE;
+	if (argc != 7)
+		return std::fprintf(
+			stderr,
+			"usage: %s <x res> <y res> <output display x-offset> <output display y-offset> <x pos> <y pos>\n",
+			argv[0]),
+			EXIT_FAILURE;
 
-    std::setbuf(stdin, nullptr), std::setbuf(stdout, nullptr);
+	std::setbuf(stdin, nullptr), std::setbuf(stdout, nullptr);
 
-    const point mon_rect       = {std::stoi(argv[1]), std::stoi(argv[2])};
-    const point display_offset = {std::stoi(argv[3]), std::stoi(argv[4])};
-    point mouse_pos            = {std::stoi(argv[5]), std::stoi(argv[6])};
-    input_event input;
+	const point mon_rect       = {std::stoi(argv[1]), std::stoi(argv[2])};
+	const point display_offset = {std::stoi(argv[3]), std::stoi(argv[4])};
+	point mouse_pos            = {std::stoi(argv[5]), std::stoi(argv[6])};
+	input_event input;
 
-    while (read_event(input)) {
-	// input.code == 1 is y axis, 0 is x axis. type 2 is mouse movements & scrollwheel events
-	if (input.type == 2) {
-		if (input.code == 1) {
-			int movement = mouse_pos.y + input.value;
-			if (movement < mon_rect.y+display_offset.y && movement > display_offset.y) {
-				write_event(input);
-				mouse_pos.y += input.value;
+	while (read_event(input)) {
+		// input.code == 1 is y axis, 0 is x axis. type 2 is mouse movements & scrollwheel events
+		if (input.type == 2) {
+			if (input.code == 1) {
+				int movement = mouse_pos.y + input.value;
+				if (movement < mon_rect.y+display_offset.y && movement > display_offset.y) {
+					write_event(input);
+					mouse_pos.y += input.value;
+				}
 			}
-		}
-		if (input.code == 0) {
-			int movement = mouse_pos.x + input.value;
-			if (movement < mon_rect.x+display_offset.x && movement > display_offset.x) {
+			if (input.code == 0) {
+				int movement = mouse_pos.x + input.value;
+				if (movement < mon_rect.x+display_offset.x && movement > display_offset.x) {
+					write_event(input);
+					mouse_pos.x += input.value;
+				}
+			} else {
 				write_event(input);
-				mouse_pos.x += input.value;
 			}
-		}
-		else
+		} else  {
 			write_event(input);
+		}
 	}
-	else 
-		write_event(input);
-    }
 }
 
